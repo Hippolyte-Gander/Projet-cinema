3de4a83ab26a17e1f1ac1aad3c68bebd2b510416
Hippolyte G <hippolyte.gander5@gmail.com>
2024-05-28 12:02:01 +0200

    connect.php --> ok début d'écriture du controller

--- connect.php
+++ connect.php
@@ -1,23 +1,22 @@
 <?php
-try {
-    // On se connecte à la BDD
-    $mysqlClient = new PDO('mysql:host=localhost;dbname=cinema_v2;charset=utf8', 'root', 'root');
-} catch (Exception $e) {
-    // En cas d'erreur, on affiche un message et on arrête tout
-    die('Erreur : ' . $e->getMessage());
-}
-// Si tout va bien, on peut continuer
 
-// On récupère tout le contenu de la table recipes
-$sqlQuery = 'SELECT * FROM recipes';
-$recipesStatement = $mysqlClient->prepare($sqlQuery);
-$recipesStatement->execute();
-$recipes = $recipesStatement->fetchAll();
+namespace Model;
 
-// On affiche chaque recette une à une
-foreach ($recipes as $recipe) {
-?>
-    <p><?php echo $recipe['author']; ?></p>
-<?php
+abstract class Connect {
+    const HOST = "localhost";
+    const DB = "cinema_v2";
+    const USER = "root";
+    const PASS = "";
+
+    public static function seConnecter(){
+        try {
+            return new \PDO(
+                "mysql:host=".self::HOST.";dbname=".self::DB.";charset=utf8", self::USER, self::PASS);
+        } catch (\PDOException $ex) {
+            return $ex-->getMessage();
+        }
+    }
 }
+
+
 ?>
---
+++ controller.php
@@ -0,0 +1,17 @@
+<?php
+include 'connect.php';
+
+
+
+
+// fonction chercher tous les films
+public function listFilms() {
+    $pdo = Connect::seConnecter();
+    $requete = $pdo-->query("
+    SELECT titre, annee_sortie
+    FROM film
+    ");
+}
+
+
+?>
